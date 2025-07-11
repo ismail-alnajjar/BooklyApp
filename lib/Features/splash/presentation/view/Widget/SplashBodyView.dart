14cfbb2bb6b752b928d532862677be036a0a1617
ismail-alnajjar <ismaillalnajjaar@gmail.com>
2025-07-11 18:50:26 +0300

    splash View ui done

--- lib/Features/splash/presentation/view/Widget/SplashBodyView.dart
+++ lib/Features/splash/presentation/view/Widget/SplashBodyView.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:lebrary/core/utils/assets.dart';
 
 class SplashBodyView extends StatelessWidget {
   const SplashBodyView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [Image.asset(AssetsData.logo)],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:lebrary/Features/splash/presentation/view/SplashView.dart';
+import 'package:lebrary/consta.dart';
 
 void main() {
   runApp(const BookyApp());
@@ -13,8 +14,14 @@ class BookyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Booky',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: KprimaryColor, //////////////////////////////
+        appBarTheme: AppBarTheme(
+          backgroundColor: KprimaryColor, /////////////////////////////////
+        ),
+      ),
       home: SplashView(),
     );
   }
